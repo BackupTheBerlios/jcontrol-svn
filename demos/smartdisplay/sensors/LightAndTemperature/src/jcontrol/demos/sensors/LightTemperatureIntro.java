8c6cfa0130d628f2c868ae25561ef711f6d5dc05
mgn <mgn@1d1dd581-9546-0410-bf79-cca000aad603>
2008-03-11 09:19:16 +0000

    new sensor BH1710

--- demos/smartdisplay/sensors/LightAndTemperature/src/jcontrol/demos/sensors/LightTemperatureIntro.java
+++ demos/smartdisplay/sensors/LightAndTemperature/src/jcontrol/demos/sensors/LightTemperatureIntro.java
@@ -1,62 +1,57 @@
 /*
- * LightTemperatureIntro.java
- * Copyright (C) 2000-2007 DOMOLOGIC Home Automation GmbH
- *
- * This library is free software; you can redistribute it and/or
- * modify it under the terms of the GNU Lesser General Public
- * License as published by the Free Software Foundation; either
- * version 2.1 of the License, or (at your option) any later version.
- *
- * This library is distributed in the hope that it will be useful,
- * but WITHOUT ANY WARRANTY; without even the implied warranty of
- * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
- * Lesser General Public License for more details.
- *
- * You should have received a copy of the GNU Lesser General Public
- * License along with this library; if not, write to the Free Software
- * Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston,
- * MA 02110-1301 USA.
+ * Copyright (C) 2000-2007 DOMOLOGIC Home Automation GmbH This library is free software; you can
+ * redistribute it and/or modify it under the terms of the GNU Lesser General Public License as
+ * published by the Free Software Foundation; either version 2.1 of the License, or (at your option)
+ * any later version. This library is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR
+ * PURPOSE. See the GNU Lesser General Public License for more details. You should have received a
+ * copy of the GNU Lesser General Public License along with this library; if not, write to the Free
+ * Software Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA.
  */
 
 package jcontrol.demos.sensors;
 
 import java.io.IOException;
+
+import jcontrol.bus.i2c.BH1710;
+import jcontrol.bus.i2c.TMP75;
+import jcontrol.bus.i2c.TSL2561;
+import jcontrol.io.Backlight;
 import jcontrol.io.Display;
 import jcontrol.io.Resource;
 import jcontrol.lang.ThreadExt;
-import jcontrol.io.Backlight;
-
-import jcontrol.bus.i2c.TMP75;
-import jcontrol.bus.i2c.TSL2561;
 
 /**
  * Intro screen for the LightTemperatureDemo.
  * 
- * @author Remi Seiler, Thomas R√∂bbenack
+ * @author Remi Seiler, Thomas Roebbenack
+ * @version $Revision$
  */
 public class LightTemperatureIntro {
 
     public LightTemperatureIntro() {
         Display d = new Display();
         d.drawString("Willkommen!", 2, 5);
-        d.drawString("Bitte schlie√üen Sie einen oder beide", 2, 25);
+        d.drawString("Bitte schlieﬂen Sie einen oder beide", 2, 25);
         d.drawString("mitgelieferten Sensoren an die", 2, 35);
-        d.drawString("daf√ºr vorgesehenen I2C-", 2, 45);
+        d.drawString("daf¸r vorgesehenen I2C-", 2, 45);
         d.drawString("Schnittstelle(n) an.", 2, 55);
 
-		try {
-			d.drawImage(new Resource("jcontrol_small.jcif"), 100, 0);
-		} catch (IOException e) {}
+        try {
+            d.drawImage(new Resource("jcontrol_small.jcif"), 100, 0);
+        } catch (IOException e) {
+        }
 
         Backlight.setBrightness(255);
-        
+
         TMP75 tmp = null;
         TSL2561 tsl = null;
-        
-        intro:for (;;) {
-        	if (tmp == null) {
-        		tmp = new TMP75(0x9e);
-        	}
+        BH1710 bh = null;
+
+        intro: for (;;) {
+            if (tmp == null) {
+                tmp = new TMP75(0x9e);
+            }
             if (tmp != null) {
                 try {
                     tmp.getTemp();
@@ -65,27 +60,38 @@ public class LightTemperatureIntro {
                 }
             }
             if (tsl == null) {
-            	try {
+                try {
                     tsl = new TSL2561(0x72);
-                } catch (IOException e2) {}
+                } catch (IOException e2) {
+                    try {
+                        bh = new BH1710(0x46);
+                    } catch (IOException e3) {
+                    }
+                }
             }
             if (tsl != null) {
                 try {
-        	    	tsl.getChannel0();
-                } catch (IOException e) { 
+                    tsl.getChannel0();
+                } catch (IOException e) {
                     tsl = null;
                 }
+            } else if (bh != null) {
+                try {
+                    bh.getContinuouslyMeasurementL();
+                } catch (IOException e) {
+                    bh = null;
+                }
             }
-            
-            if (tmp != null || tsl != null) {
-            	break intro;
+
+            if (tmp != null || tsl != null || bh != null) {
+                break intro;
             }
-            
+
             try {
                 ThreadExt.sleep(1);
             } catch (InterruptedException e) {
             }
-		}
+        }
         d.clearDisplay();
     }
 }
