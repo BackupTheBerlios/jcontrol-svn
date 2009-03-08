e49d856b8855f8863925755417d73da4266f1a1e
mgn <mgn@1d1dd581-9546-0410-bf79-cca000aad603>
2009-03-08 14:38:15 +0000

    Change year

--- trunk/extlib/src/java/jcontrol/san/interfaces/sensors/Temperature.java
+++ trunk/extlib/src/java/jcontrol/san/interfaces/sensors/Temperature.java
@@ -1,20 +1,20 @@
 /*
- * Copyright (C) 2008 The JControl Group and individual authors listed below
- *
- * This library is free software; you can redistribute it and/or modify it
- * under the terms of the GNU Lesser General Public License as published by the
- * Free Software Foundation; either version 2.1 of the License, or (at your
- * option) any later version.
- *
+ * Copyright (C) 2008-2009 The JControl Group and individual authors listed
+ * below.
+ * 
+ * This library is free software; you can redistribute it and/or modify it under
+ * the terms of the GNU Lesser General Public License as published by the Free
+ * Software Foundation; either version 2.1 of the License, or (at your option)
+ * any later version.
+ * 
  * This library is distributed in the hope that it will be useful, but WITHOUT
- * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or
- * FITNESS FOR A PARTICULAR PURPOSE. See the GNU Lesser General Public License
- * for more details.
- *
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE. See the GNU Lesser General Public License for more
+ * details.
+ * 
  * You should have received a copy of the GNU Lesser General Public License
- * along with this library; if not, write to the Free Software Foundation,
- * Inc., 59 Temple Place, Suite 330, Boston, MA 02111-1307 USA
- *
+ * along with this library; if not, write to the Free Software Foundation, Inc.,
+ * 59 Temple Place, Suite 330, Boston, MA 02111-1307 USA
  */
 
 package jcontrol.san.interfaces.sensors;
@@ -29,12 +29,12 @@ import java.io.IOException;
  */
 public interface Temperature extends Sensor {
 
-    /**
-     * Reads a temperature from the sensor and store it.
-     * 
-     * @return the temperature from the sensor.
-     * @throws IOException if a IO-error is occurred.
-     */
-    int getTemp() throws IOException;
+   /**
+    * Reads a temperature from the sensor and store it.
+    * 
+    * @return the temperature from the sensor.
+    * @throws IOException if a IO-error is occurred.
+    */
+   int getTemp() throws IOException;
 
 }
